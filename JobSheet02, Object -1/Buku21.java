71cf8a642786c52e47098143005bad41d6dd2b3d
afiqfirdaus21 <144087118+afiqfirdaus21@users.noreply.github.com>
2024-02-22 23:39:30 +0700

    Add files via upload

--- JobSheet02, Object -1/Buku21.java
+++ JobSheet02, Object -1/Buku21.java
@@ -1,7 +1,16 @@
 public class Buku21 {
     String judul, pengarang;
     int halaman, stok, harga;
+    public Buku21(){
 
+    }
+    public Buku21 (String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
     void tampilInformasi() {
         System.out.println("Judul : " + judul);
         System.out.println("Pengarang : " + pengarang);
@@ -10,12 +19,31 @@ public class Buku21 {
         System.out.println("Harga : Rp " + harga);
     }
     void terjual(int jml) {
-        stok -= jml;
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("Stok habis!");
+        }
     }
     void restock(int jml) {
         stok += jml;
     }
     void gantiHarga(int hrg) {
         harga = hrg;
+    }
+    int hitungHargaTotal(int jumlahTerjual) {
+        return harga * jumlahTerjual;
+    }
+    int hitungDiskon(int hargaTotal) {
+        if (hargaTotal > 150000) {
+            return hargaTotal * 12 / 100;
+        } else if (hargaTotal >= 75000 && hargaTotal <= 150000) {
+            return hargaTotal * 5 / 100;
+        } else {
+            return 0;
+        }
+    }
+    int hitungHargaBayar(int hargaTotal, int diskon) {
+        return hargaTotal - diskon;
     }
 }
---
+++ JobSheet02, Object -1/BukuMain21.java
@@ -0,0 +1,28 @@
+public class BukuMain21 {
+    public static void main(String[] args) {
+        Buku21 bk1 = new Buku21();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denada Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+
+        Buku21 bk2 = new Buku21("Self Reward", "Mahendra Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
+
+        Buku21 bukuAfiq = new Buku21("Laskar Pelangi", "Andrea Hirata", 529, 100, 79200);
+        bukuAfiq.terjual(18);
+        bukuAfiq.tampilInformasi();
+
+        int jumlahTerjual = 5;
+        int hargaTotal = bukuAfiq.hitungHargaTotal(jumlahTerjual);
+        int diskon = bukuAfiq.hitungDiskon(hargaTotal);
+        int hargaBayar = bukuAfiq.hitungHargaBayar(hargaTotal, diskon);
+    }
+}
